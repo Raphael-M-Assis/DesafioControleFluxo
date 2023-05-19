bd5eb856a5c0df2e107a3e9a6c3f7ce5ec6c636a
Raphael-M-Assis <raphaelmoreira120@gmail.com>
2023-05-19 13:27:36 -0300

    fix: alteracao no filtro de erro

--- src/Contator.java
+++ src/Contator.java
@@ -12,10 +12,14 @@ public class Contator {
         } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
+
+        terminal.close();
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        if (parametroUm > parametroDois) {
+        //adicionei a validacao de maior ou igual porque nao faz sentido a mensagem
+        //de "o segundo parametro deve ser maior que o primeiro" e nao acusar erro ao serem iguais
+        if (parametroUm >= parametroDois) {
             throw new ParametrosInvalidosException();
         }
 
