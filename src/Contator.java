77ff0db05b0863498aa74d72ba9c8ba52d3e0dab
Raphael-M-Assis <raphaelmoreira120@gmail.com>
2023-05-19 13:19:45 -0300

    fix: exercicio controle de fluxo

---
+++ src/Contator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Contator {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println(i);
+        }
+    }
+}
